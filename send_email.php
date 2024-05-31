73d906ec8e6051c8cf629cadc48fced2aa31fcc7
vladis112233 <124692818+vladis112233@users.noreply.github.com>
2024-05-31 21:20:25 +0300

    13

---
+++ send_email.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nameRestaurant = htmlspecialchars($_POST['nameRestaurant']);
+    $adress = htmlspecialchars($_POST['adress']);
+    $email = htmlspecialchars($_POST['email']);
+    $phone = htmlspecialchars($_POST['phone']);
+    $turnover = htmlspecialchars($_POST['turnover']);
+    $name = htmlspecialchars($_POST['name']);
+
+    $to = "grachev01112000@gmail.com";
+    $subject = "Анкета з сайту";
+    $message = "Назва ресторану: $nameRestaurant\nАдреса: $adress\nEmail: $email\nТелефон: $phone\nСередній обсяг олії в місяць: $turnover\nІм'я: $name";
+    $headers = "From: webmaster@example.com";
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Анкету успішно відправлено!";
+    } else {
+        echo "Помилка під час відправлення анкети.";
+    }
+} else {
+    echo "Неправильний запит.";
+}
+?>
